3bdbe0ebf86cb14de8d10d80c9e11a00d7179b65
jvcostaf <joaovcosta220@gmail.com>
2024-03-30 11:09:38 -0300

    iniciando o repositorio

---
+++ src/main/java/com/algorithms/graphs/GraphsApplication.java
@@ -0,0 +1,13 @@
+package com.algorithms.graphs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/algorithms/graphs/bfs/MainApp.java
@@ -0,0 +1,7 @@
+package com.algorithms.graphs.bfs;
+
+public class MainApp {
+    public static void main(String[] args) {
+        
+    }
+}
